c191fb78e109b7da43f8abb4c99589bd2f2c0b1b
analauracano <laura.fioratti@outlook.com>
2025-10-12 15:56:55 +0100

    docker, sequelize, migrate

---
+++ src/config/database.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'Largatixa1*',
+    database: 'burger-db',
+    define: {
+        timeStamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
