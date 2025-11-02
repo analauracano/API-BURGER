76b4ca233da93df1b20f14702b1a628a983794e6
analauracano <laura.fioratti@outlook.com>
2025-11-02 21:54:54 +0000

    orders e finalização

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,10 @@ import SessionController from "./app/controllers/SessionController.js";
 import ProductController from "./app/controllers/ProductController.js";
 import multer from "multer";
 import multerConfig from "./config/multer.cjs";
-import authMiddleware from "./middlewares/auth.js";
+import authMiddleware from "./app/middlewares/auth.js";
 import CategoryController from "./app/controllers/CategoryController.js";
-import adminMiddleware from "./middlewares/admin.js";
+import adminMiddleware from "./app/middlewares/admin.js";
+import OrderController from "./app/controllers/OrderController.js";
 
 const routes = new Router();
 
@@ -24,5 +25,9 @@ routes.get('/products', ProductController.index);
 routes.post('/categories', adminMiddleware, upload.single('file'), CategoryController.store);
 routes.put('/categories/:id', adminMiddleware, upload.single('file'), CategoryController.update);
 routes.get('/categories', CategoryController.index);
+
+routes.post('/orders', OrderController.store);
+routes.put('/orders/:id', adminMiddleware, OrderController.update);
+routes.get('/orders', OrderController.index);
 
 export default routes;
