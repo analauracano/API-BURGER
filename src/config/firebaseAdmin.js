0896561c3afe89bf47942938ccba3368e964e0a9
analauracano <laura.fioratti@outlook.com>
2025-12-05 18:09:46 +0000

    firebase

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 
-// Lê o JSON do Service Account da variável de ambiente
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// Corrige quebras de linha para o Firebase ler corretamente
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
